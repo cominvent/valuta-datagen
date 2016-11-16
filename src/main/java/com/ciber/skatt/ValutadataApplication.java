e15a47ecb7cfd4d785ccb566acaea82ea92d4e23
Jan Høydahl <jan.git@cominvent.com>
2016-11-16 15:52:13 +0100

    Better command line args

--- src/main/java/com/ciber/skatt/ValutadataApplication.java
+++ src/main/java/com/ciber/skatt/ValutadataApplication.java
@@ -9,38 +9,39 @@ public class ValutadataApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-    String type;
-    long seed;
-    long rows;
-		if (args.length == 3) {
-      type = args[0];
-      seed = Long.parseLong(args[1]);
-      rows = Long.parseLong(args[2]);
-		} else {
-			System.err.println("Usage: valuta <type> <seed> <rows>");
-      System.err.println("Using defaults");
-      type = "trans";
-      seed = 123;
-      rows = 5;
+    String type = "trans";
+    long seed = 1;
+    long rows = 1;
+    switch (args.length) {
+      case 3:
+        seed = Long.parseLong(args[2]);
+      case 2:
+        type = args[1];
+      case 1:
+        rows = Long.parseLong(args[0]);
+        break;
+      default:
+        System.err.println("Usage: valuta <rows> [<type>] [<seed>]");
+        System.exit(0);
     }
 
-		switch (type) {
-			case "trans":
-				TransGenerator tg = TransGenerator.getInstance();
+    switch (type) {
+      case "trans":
+        TransGenerator tg = TransGenerator.getInstance();
         tg.setSeed(seed);
         tg.init(new TransGenerator.Config());
-        for (int i=0 ; i < rows; i++) {
+        for (int i = 0; i < rows; i++) {
           System.out.println(tg.generate());
         }
-				break;
-			case "declare":
-				break;
-			default:
-				System.err.println("Unknown type " + type);
-				System.exit(2);
-				break;
-		}
-	}
+        break;
+      case "declare":
+        break;
+      default:
+        System.err.println("Unknown type " + type);
+        System.exit(2);
+        break;
+    }
+  }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ValutadataApplication.class, args);
