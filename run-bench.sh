1986e2eef82282fbe74265ff547cf694a38f1c64
Jan Høydahl <jan.git@cominvent.com>
2017-06-12 08:56:59 +0200

    Only require yellow

--- run-bench.sh
+++ run-bench.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-esrally --pipeline=benchmark-only --target-hosts=17.0.0.1:9200 --track-repository=valutareg-rally \
+esrally --pipeline=benchmark-only --target-hosts=17.0.0.1:9200 --track-repository=valutareg-rally --cluster-health=yellow \
   --track=valutareg --challenge=query
